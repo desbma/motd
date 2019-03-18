806b65a0f330f189025c1b094b382a4a1640ad8d
desbma <desbma@users.noreply.github.com>
2019-03-18 01:27:52 +0100

    Initial memory display

--- src/mem.rs
+++ src/mem.rs
@@ -3,6 +3,11 @@ use std::io::{BufReader,BufRead};
 use std::fs::File;
 use std::str::FromStr;
 
+use colored::*;
+
+
+const MEMBAR_LENGTH : u64 = 70;
+
 
 /// Map of memory usage info
 pub type MemInfo = HashMap<String, u64>;
@@ -26,7 +31,17 @@ pub fn get_mem_info(mem_info: &mut MemInfo) {
 
 
 pub fn output_mem(mem_info: MemInfo) {
-    for (key, val) in mem_info {
-        println!("{}={}", key, val);
-    }
+    let total_mem_mb = mem_info["MemTotal"] / 1024;
+    let cache_mem_mb = mem_info["Cached"] / 1024;
+    let buffer_mem_mb = mem_info["Buffers"] / 1024;
+    let free_mem_mb = mem_info["MemFree"] / 1024;
+    let used_mem_mb  = total_mem_mb - cache_mem_mb - buffer_mem_mb - free_mem_mb;
+
+    println!("{:.1} ({:.1}%) [{}{}{}] {:.1}GB ",
+             used_mem_mb as f64 / 1024.0,
+             100.0 * used_mem_mb as f64 / total_mem_mb as f64,
+             "█".repeat((MEMBAR_LENGTH * used_mem_mb / total_mem_mb) as usize),
+             "█".repeat((MEMBAR_LENGTH * (cache_mem_mb + buffer_mem_mb) / total_mem_mb) as usize).dimmed(),
+             " ".repeat((MEMBAR_LENGTH * free_mem_mb / total_mem_mb) as usize),
+             total_mem_mb as f64 / 1024.0);
 }
